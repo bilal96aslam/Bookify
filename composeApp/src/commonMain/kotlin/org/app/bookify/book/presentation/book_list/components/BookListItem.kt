f3ad65796a77835bdadf4f911e7c04469ab81386
bilal96aslam <bilalaslamch96@gmail.com>
2025-01-22 12:31:21 +0500

    remove unused import

--- composeApp/src/commonMain/kotlin/org/app/bookify/book/presentation/book_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/org/app/bookify/book/presentation/book_list/components/BookListItem.kt
@@ -15,9 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
