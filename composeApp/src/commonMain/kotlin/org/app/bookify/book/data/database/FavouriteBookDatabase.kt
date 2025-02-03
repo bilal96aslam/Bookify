933983228d1e84d82702f7e911498cf68173827a
bilal96aslam <bilalaslamch96@gmail.com>
2025-02-03 18:45:00 +0500

    minor update for Ios app

--- composeApp/src/commonMain/kotlin/org/app/bookify/book/data/database/FavouriteBookDatabase.kt
+++ composeApp/src/commonMain/kotlin/org/app/bookify/book/data/database/FavouriteBookDatabase.kt
@@ -1,12 +1,14 @@
 package org.app.bookify.book.data.database
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(entities = [BookEntity::class], version = 1)
 @TypeConverters(StringListTypeConverter::class)
-abstract class  FavouriteBookDatabase : RoomDatabase() {
+@ConstructedBy(BookDatabaseConstructor::class)
+abstract class FavouriteBookDatabase : RoomDatabase() {
     abstract val favouriteBookDao: FavouriteBookDao
 
     companion object {
